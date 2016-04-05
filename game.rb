4f6997f18258c57f21b08d4c22be4ac458dafc6f
Toby Clarke <teeohbee@gmail.com>
2016-04-05 21:17:47 +0100

    Refactored classes and code

--- game.rb
+++ game.rb
@@ -1,30 +1,23 @@
 require 'gosu'
+require_relative 'lib/star.rb'
+require_relative 'lib/player.rb'
 
 class GameWindow < Gosu::Window
   def initialize
     super 640, 480
     self.caption = "Gosu Tutorial Game"
-
-    @background_image = Gosu::Image.new("space.png", tileable: true)
-
+    @background_image = Gosu::Image.new("media/space.png", tileable: true)
     @player = Player.new
     @player.warp(320, 240)
-
-    @star_anim = Gosu::Image::load_tiles("star.png", 25, 25)
+    @star_anim = Gosu::Image::load_tiles("media/star.png", 25, 25)
     @stars = Array.new
     @font = Gosu::Font.new(20)
   end
 
   def update
-    if Gosu::button_down? Gosu::KbLeft then
-      @player.turn_left
-    end
-    if Gosu::button_down? Gosu::KbRight then
-      @player.turn_right
-    end
-    if Gosu::button_down? Gosu::KbUp  then
-      @player.accelerate
-    end
+    @player.turn_left if Gosu::button_down? Gosu::KbLeft
+    @player.turn_right if Gosu::button_down? Gosu::KbRight
+    @player.accelerate if Gosu::button_down? Gosu::KbUp
     @player.move
     @player.collect_stars(@stars)
 
@@ -47,87 +40,8 @@ class GameWindow < Gosu::Window
   end
 end
 
-class Player
-  attr_reader :score
-
-  def initialize
-    @image = Gosu::Image.new("starfighter.bmp")
-    @beep = Gosu::Sample.new("beep.wav")
-    @x = @y = @vel_x = @vel_y = @angle = 0.0
-    @score = 0
-  end
-
-  def warp(x, y)
-    @x = x
-    @y = y
-  end
-
-  def turn_left
-    @angle -= 4.5
-  end
-
-  def turn_right
-    @angle += 4.5
-  end
-
-  def accelerate
-    @vel_x += Gosu::offset_x(@angle, 0.5)
-    @vel_y += Gosu::offset_y(@angle, 0.5)
-  end
-
-  def move
-    @x += @vel_x
-    @y += @vel_y
-    @x %=  640
-    @y %= 480
-
-    @vel_x *= 0.95
-    @vel_y *= 0.95
-  end
-
-  def score
-    @score
-  end
-
-  def collect_stars(stars)
-      stars.reject! do |star|
-        if Gosu::distance(@x, @y, star.x, star.y) < 35 then
-          @score += 10
-          @beep.play
-          true
-        else
-          false
-        end
-      end
-    end
-
-  def draw
-    @image.draw_rot(@x, @y, 1, @angle)
-  end
-end
-
 module ZOrder
   Background, Stars, Player, UI = *0..3
-end
-
-class Star
-  attr_reader :x, :y
-
-  def initialize(animation)
-    @animation = animation
-    @color = Gosu::Color.new(0xff_000000)
-    @color.red = rand(256 - 40) + 40
-    @color.green = rand(256 - 40) + 40
-    @color.blue = rand(256 - 40) + 40
-    @x = rand * 640
-    @y = rand * 480
-  end
-
-  def draw
-    img = @animation[Gosu::milliseconds / 100 % @animation.size];
-    img.draw(@x - img.width / 2.0, @y - img.height / 2.0,
-      ZOrder::Stars, 1, 1, @color, :add)
-  end
 end
 
 window = GameWindow.new
