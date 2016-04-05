e7432d420ab0f9013a7ba6974a82c85dc19b53fe
Toby Clarke <teeohbee@gmail.com>
2016-04-05 20:10:15 +0100

    Added star class and integrated into game

--- game.rb
+++ game.rb
@@ -9,6 +9,9 @@ class GameWindow < Gosu::Window
 
     @player = Player.new
     @player.warp(320, 240)
+
+    @star_anim = Gosu::Image::load_tiles("star.png", 25, 25)
+    @stars = Array.new
   end
 
   def update
@@ -22,11 +25,17 @@ class GameWindow < Gosu::Window
       @player.accelerate
     end
     @player.move
+    @player.collect_stars(@stars)
+
+    if rand(100) < 4 and @stars.size < 25 then
+      @stars.push(Star.new(@star_anim))
+    end
   end
 
   def draw
-    @background_image.draw(0, 0, 0)
+    @background_image.draw(0, 0, ZOrder::Background)
     @player.draw
+    @stars.each { |star| star.draw }
   end
 
   def button_down(id)
@@ -71,8 +80,42 @@ class Player
     @vel_y *= 0.95
   end
 
+  def score
+    @score
+  end
+
+  def collect_stars(stars)
+    if stars.reject! {|star| Gosu::distance(@x, @y, star.x, star.y) < 35 } then
+      @score += 1
+    end
+  end
+
   def draw
     @image.draw_rot(@x, @y, 1, @angle)
+  end
+end
+
+module ZOrder
+  Background, Stars, Player, UI = *0..3
+end
+
+class Star
+  attr_reader :x, :y
+
+  def initialize(animation)
+    @animation = animation
+    @color = Gosu::Color.new(0xff_000000)
+    @color.red = rand(256 - 40) + 40
+    @color.green = rand(256 - 40) + 40
+    @color.blue = rand(256 - 40) + 40
+    @x = rand * 640
+    @y = rand * 480
+  end
+
+  def draw
+    img = @animation[Gosu::milliseconds / 100 % @animation.size];
+    img.draw(@x - img.width / 2.0, @y - img.height / 2.0,
+      ZOrder::Stars, 1, 1, @color, :add)
   end
 end
 
