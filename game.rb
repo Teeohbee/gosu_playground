5342375dadc87fffe92ffb50fcfd9a84ec848c0f
Toby Clarke <teeohbee@gmail.com>
2016-04-05 19:16:57 +0100

    Game now loads and draws background image

--- game.rb
+++ game.rb
@@ -4,12 +4,15 @@ class GameWindow < Gosu::Window
   def initialize
     super 640, 480
     self.caption = "Gosu Tutorial Game"
+
+    @background_image = Gosu::Image.new("space.png", tileable: true)
   end
 
   def update
   end
 
   def draw
+    @background_image.draw(0, 0, 0)
   end
 end
 
